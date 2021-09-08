5957bcd37e9e15721000c60b750435dd8304798f
Venezia Benjamin <benjamin.venezia@golem.ai>
2021-09-08 12:10:57 +0200

    Add an search book, delete book, panels function, hide booksto the project

--- app.js
+++ app.js
@@ -1,20 +1,93 @@
+const btns = document.querySelectorAll('#book-list ul li .delete');
+//méthode trop coûtese, on attache
+//un événement par bouton. 
+// btns.forEach(btn => {
+//    btn.addEventListener('click', (e) => {
+//         const li = e.target.parentElement;
+//         li.parentNode.removeChild(li);
+//    })
+// });
+
+//On préférera compter sur l'event Bubbling. 
+//On attache l'évenement sur le parent 
+//c'est uniquement lorsque l'élément enfant
+//sera cliqué, qu'on fera remonter l'évenement.
+
+//delete book
 const list = document.querySelector('#book-list ul');
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete'){
+        //on trouve la parent du bouton
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
+})
 
-// delete books
-list.addEventListener('click', (e) => {
-  if(e.target.className == 'delete'){
-    const li = e.target.parentElement;
-    console.log('child element to remove:', li);
-    console.log('parent element to remove child from:', li.parentElement);
-    li.parentNode.removeChild(li);
-  }
-});
+//add books 
+const addForm = document.forms['add-book'];
+addForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
 
-// prevent default behaviour
+    //create element
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
 
-const link = document.querySelector('#page-banner a');
+    //add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
 
-link.addEventListener('click', function(e){
-  e.preventDefault();
-  console.log('Navigation to', e.target.textContent, 'was prevented');
-});
+    //add classes
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+    //append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+})
+
+//hide books
+
+const hideBox = document.getElementById('hide');
+
+hide.addEventListener('change', ()=> {
+    if(hideBox.checked) {
+        list.style.display = "none";
+    } else {
+        list.style.display = "initial";
+    }
+})
+
+//filter books
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', (e) => {
+    const term = e.target.value.toLowerCase(); // texte
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach((book)=>{
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(term) != -1){
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    })
+})
+
+//tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', (e)=>{
+    if(e.target.tagName == "LI"){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach(function(panel){
+            if(panel == targetPanel){
+                panel.classList.add('active');
+            }else {
+                panel.classList.remove('active');
+            }
+        })
+    }
+}) 
